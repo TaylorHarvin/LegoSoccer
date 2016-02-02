c5c88f08ad353a48724255cb98279ac0ebbd8856
Taylor <tharvin12@gmail.com>
2016-02-02 09:08:06 -0600

    new

---
+++ NEWEST/src/FullSoccer/ColorDetector.java
@@ -0,0 +1,49 @@
+/* Programmer:	Taylor Harvin
+ * Date Last Modified: 7/19/2015
+ * Purpose:		This controls color monitoring of the floor.
+ * 				This helps with location detection of the robot.
+ */
+
+package FullSoccer;
+
+import lejos.hardware.port.Port;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.Color;
+import lejos.robotics.SampleProvider;
+
+public class ColorDetector {
+	private EV3ColorSensor colorSensor;		// Main color sensor
+	private SampleProvider colorDataProvider;// Data collector from the color sensor
+	private float[] colorSample;				// Result of data collection from the color sensor
+	
+	// Constructor for the ColorDetector
+	public ColorDetector(Port colorSensorPort) {
+		colorSensor = new EV3ColorSensor(colorSensorPort);
+		colorDataProvider = colorSensor.getColorIDMode();
+		colorSample = new float[colorDataProvider.sampleSize()];
+	}
+	
+	// Check if the robot is in a shooting range of the goal (or if goalie is still in the goal)	
+	public boolean atEdgeOfGoal(){
+		colorDataProvider.fetchSample(colorSample, 0);
+		//System.out.println("Color:" + colorSample);
+		if((int) colorSample[0] == 2 /*|| colorSample[0] == Color.GREEN*/)
+			return true;
+		else
+			return false;
+	}
+	
+	// Check if the robot is at the edge of the board	
+	public boolean atEdgeOfBoard(){
+		colorDataProvider.fetchSample(colorSample, 0);
+		if((int) colorSample[0] == Color.BLACK /*|| colorSample[0] == Color.GREEN*/)
+			return true;
+		else
+			return false;
+	}
+
+	public void stop(){
+		colorSensor.close();
+	}
+}
---
+++ NEWEST/src/FullSoccer/GoalFinder.java
@@ -0,0 +1,45 @@
+package FullSoccer;
+
+import lejos.robotics.navigation.Navigator;
+import lejos.utility.Delay;
+
+
+public class GoalFinder {
+	private Navigator nav;
+	private final float goalX = (float) (54*.775);
+	private final float goalY = (float) (-42*.775);
+	private ColorDetector colorDetector;
+	private float xPosOffset = 0;
+	private float yPosOffset = 0;
+	
+	public GoalFinder(Navigator newNav, ColorDetector cDetect, float xOffset,float yOffset){
+		nav = newNav;
+		colorDetector = cDetect;
+		xPosOffset = xOffset;
+		//yPosOffset = yOffset;
+		// Client player
+		if(xOffset == 0 && yOffset == 0){
+			xPosOffset = goalX;
+			yPosOffset = goalY;
+		}
+		// Server player
+		else{
+			xPosOffset = (float) (40.0*.775);
+			yPosOffset = (float) ((27.0-5)*0.775);
+		}
+		/*if(yOffset < goalY){
+			//yPosOffset = Math.abs((Math.abs(goalY)-Math.abs(yOffset)));
+			yPosOffset = ;
+		}*/
+		
+	}
+	
+	// Go to the goal
+	public void gotoGoal(){
+		//nav.goTo(goalX+xPosOffset,goalY+yPosOffset);
+		nav.goTo(xPosOffset,yPosOffset);
+		while(nav.isMoving() /*&& !colorDetector.atEdgeOfGoal()*/){
+			Delay.msDelay(50);
+		}
+	}
+}
